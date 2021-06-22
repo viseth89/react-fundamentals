2feb4abe16c5e2062d20b17c26744bed15138e07
viseth89 <viseth89@gmail.com>
2021-06-22 17:39:47 -0500

    Finished 1.5.4

---
+++ src/javascript_practice/super.js
@@ -0,0 +1,27 @@
+class User {
+    constructor(name) {
+        this.name = name;
+        this.type = 'Trial User'
+    };
+
+    // Method 1
+    greet() {
+        console.log(`Welcome back, ${this.name}`);
+    };
+
+    // Method 2
+    status() {
+        console.log(`Current status: ${this.type}`);
+    };
+};
+
+class TrialUser extends User {
+    trialEnding() {
+        console.log(`Your trial will be ending soon, ${this.name}.  Would you like to join our program?`)
+    }
+}
+
+// Instance of User class
+let anonDude = new User('Anonymous');
+anonDude.greet();
+anonDude.status();
