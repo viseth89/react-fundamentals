7970cad4c008f880262250d7cfadebe5ea1998fd
viseth89 <viseth89@gmail.com>
2021-06-24 06:54:18 -0500

    2.3 Single Div Rule

--- src/components/components/FunctionalComponents/FunctionalComponentDemo.js
+++ src/components/components/FunctionalComponents/FunctionalComponentDemo.js
@@ -1,7 +1,11 @@
 const FunctionalComponentDemo = () => {
   return (
     <div className="main">
-      <div className="mainDiv">Hello React</div>
+      <div className="mainDiv">Hello React
+      </div>
+      <div className="mainDiv2">Hello React
+      </div>
+
     </div>
   );
 };
---
+++ src/javascript_practice/destructuring.js
@@ -0,0 +1,2 @@
+const array = ['thing', 42, false, {key: 'value'}, [1,2,3,4,undefined], NaN];
+
