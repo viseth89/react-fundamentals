887f3d293a3af2b90ff8e15713467be732f89765
viseth89 <viseth89@gmail.com>
2021-06-24 07:48:13 -0500

    Working on 2.5, trying to understand dl, dt and dd and their relationship

--- src/components/components/FunctionalComponents/FunctionalComponentDemo.js
+++ src/components/components/FunctionalComponents/FunctionalComponentDemo.js
@@ -2,10 +2,13 @@ const FunctionalComponentDemo = () => {
   return (
     <div className="main">
       <div className="mainDiv">Hello React
+      <h1>Functional Component</h1>
+      <p>Functional Components are the primary tool in React to build a small, modular piece of your page.</p>
+      <dl>
+        <dt>Can use state</dt>
+        <dd> With tthe 'useSttate' hook, functional components can now botth render a display to the page and update the information to be shown.</dd>
+      </dl>
       </div>
-      <div className="mainDiv2">Hello React
-      </div>
-
     </div>
   );
 };
