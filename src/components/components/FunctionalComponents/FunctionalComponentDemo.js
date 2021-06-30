75f5adbe5465d641c6580d1977099f66c14087a3
viseth89 <viseth89@gmail.com>
2021-06-30 18:29:05 -0500

    Finished 2.7

--- src/components/components/FunctionalComponents/FunctionalComponentDemo.js
+++ src/components/components/FunctionalComponents/FunctionalComponentDemo.js
@@ -1,27 +1,111 @@
+import {
+  Card,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Button,
+  Container,
+  Row,
+  Col,
+} from "reactstrap";
+
 const FunctionalComponentDemo = () => {
   return (
-    <div className="main">
-      <div className="mainDiv">
-      <h1>Functional Component</h1>
-      <p>Functional Components are the primary tool in React to build a small, modular piece of your page.</p>
-      <dl>
-        <dt>Can use state</dt>
-        <dd> With tthe 'useSttate' hook, functional components can now botth render a display to the page and update the information to be shown.</dd>
+    <Container className="main">
+      <Row>
+        <Col xs="12">
+          <h1>Functional Component</h1>
+          <p>
+            Functional Components are the primary tool in React to build a
+            small, modular piece of your page.
+          </p>
+          <dl>
+            <dt>Can use state</dt>
+            <dd>
+              {" "}
+              With tthe 'useSttate' hook, functional components can now botth
+              render a display to the page and update the information to be
+              shown.
+            </dd>
 
-        <dt>No 'this' keyword</dt>
-        <dd>Older class components in React use 'this'.  Functional components have no 'this' object.</dd>
+            <dt>No 'this' keyword</dt>
+            <dd>
+              Older class components in React use 'this'. Functional components
+              have no 'this' object.
+            </dd>
 
-        <dt>Can use effects</dt>
-        <dd>With tthe 'useEffect' hook, functional components can perform side effects with any props or state changes.</dd>
+            <dt>Can use effects</dt>
+            <dd>
+              With tthe 'useEffect' hook, functional components can perform side
+              effects with any props or state changes.
+            </dd>
 
-        <dt>return()</dt>
-        <dd>Must return a single element, but thtis element may have nested elements inside.</dd>
-      </dl>
-
+            <dt>return()</dt>
+            <dd>
+              Must return a single element, but thtis element may have nested
+              elements inside.
+            </dd>
+          </dl>
+        </Col>
+      </Row>
+<hr />
+<h1>Challenge 1</h1>
       <h1>Functional syntas versus Arrow Function</h1>
-      </div>
-    </div>
+      <Row>
+        <Col md="6">
+          <HelloWorldFatArrow className="logo" />
+        </Col>
+        <Col md="6">
+          <HelloWorld />
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
 export default FunctionalComponentDemo;
+
+const HelloWorld = function () {
+  return (
+    <div>
+      <Card>
+        <img
+          width="100%"
+          height="280px"
+          src="https://i.ytimg.com/vi/BwAakF_VUV8/maxresdefault.jpg"
+          alt="card cap"
+        />
+        <CardBody>
+          <CardTitle>Regular Ole Function</CardTitle>
+          <CardSubtitle>A JS Library</CardSubtitle>
+          <CardText>
+            <pre>const HelloWorld = function()</pre>
+          </CardText>
+          <Button>Go somewhere, yo</Button>
+        </CardBody>
+      </Card>
+    </div>
+  );
+};
+
+const HelloWorldFatArrow = () => (
+  <div>
+    <Card>
+      <img
+        width="100%"
+        height="280px"
+        src="https://i.ytimg.com/vi/_pfXEv9cFGE/maxresdefault.jpg"
+        alt="card cap"
+      />
+      <CardBody>
+        <CardTitle>Fat Arrow</CardTitle>
+        <CardSubtitle>A JS Library</CardSubtitle>
+        <CardText>
+          <pre>const HelloWorld = () => </pre>
+        </CardText>
+        <Button>Go somehwere man</Button>
+      </CardBody>
+    </Card>
+  </div>
+);
