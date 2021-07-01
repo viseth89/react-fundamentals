6287fb87997d40601f8150a46adc6e8f10dad0d2
viseth89 <viseth89@gmail.com>
2021-07-01 11:11:41 -0500

    Finished 6.4 starting to see how props can be used attemping challenge on 6.5

--- src/components/concepts/PropsDemo.js
+++ src/components/concepts/PropsDemo.js
@@ -1,14 +1,38 @@
 import {useState} from 'react';
 
 const PropsDemo = () => {
+    const [ color, setColor ] = useState('white');
+    const [ backgroundColor, setBackgroundColor ] = useState('purple');
+    const [ borderRadius, setBorderRadius ] = useState('5px');
+    const [ borderStyle, setBorderStyle ] = useState('dashed');
+    const [ display, setDisplay ] = useState('inline-block');
+    const [ width, setWidth ] = useState('350px');
+    const [textAlign, setTextAlign ] = useState('center');
+
+    let styles={
+        color: color,
+        backgroundColor: backgroundColor,
+        borderRadius: borderRadius,
+        borderStyle: borderStyle,
+        display: display,
+        width: width,
+        textAlign :  textAlign
+    }
+
+    const toggleColor = () => {
+        color === 'white' ? setColor('pink') : setColor('white');
+    }
+
     return(
         <div className='main'>
             <div className='mainDiv'>
-                <FunctionalComponent string='will this display?' />
-                <FunctionalComponent string="props are pretty cool, right?" />
-                <FunctionalComponent string='you can send data from one component...' />
-                <FunctionalComponent string='... to another!' />
+                <div style={styles}>
+                <FunctionalComponent string='will this display?' function={toggleColor}/>
+                <FunctionalComponent string="props are pretty cool, right?" function={toggleColor}/>
+                <FunctionalComponent string='you can send data from one component...' function={toggleColor}/>
+                <FunctionalComponent string='... to another!' function={toggleColor}/>
                 {/* I find this extremely strange and have not yet picked up the pattern nor do I see how it is effective/effeicent */}
+                </div>
             </div>
         </div>
     )
@@ -20,6 +44,7 @@ const FunctionalComponent = (props) => {
     return(
         <div>
             <p>{props.string}</p>
+            <button onClick={props.function}>Press Me!</button>
         </div>
     );
 };
